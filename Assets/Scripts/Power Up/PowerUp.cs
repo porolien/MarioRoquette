7691cbda2effed3f0bdf6c8062b24f0dca5cd254
AsaeNeopie <n.noahchery@gmail.com>
2023-12-13 15:09:22 +0100

    Power up qui sort du bloque + le prendre en main

--- Assets/Scripts/Power Up/PowerUp.cs
+++ Assets/Scripts/Power Up/PowerUp.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PowerUp : MonoBehaviour
+public class PowerUp : DynamicObject
 {
+
+    private void Awake()
+    {
+        SetUpPhysics();
+    }
+
+    void LateUpdate()
+    {
+        UpdatePhysics();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(other.name);
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("T'a graille le powerUP FDP");
+            Destroy(gameObject);
+        }
     }
 }
