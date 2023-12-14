02d44c851411b565f3d73e728c3864e49e4b5b70
AsaeNeopie <n.noahchery@gmail.com>
2023-12-14 16:34:22 +0100

    Je vais craquer mon slibard

--- Assets/Scripts/Power Up/ScaleRocketScript.cs
+++ Assets/Scripts/Power Up/ScaleRocketScript.cs
@@ -7,7 +7,7 @@ public class ScaleRocketScript : PowerUp
     protected override void PowerUpEffect()
     {
         RocketManager.Instance.rocketMove.RayonDeLexplosion *= 2f;
-        RocketManager.Instance.rocketMove.TailleRocket *= 10;
+        RocketManager.Instance.rocketMove.TailleDeLaRocket();
         RocketManager.Instance.rocketMove.multiplicateurDeLexplosion *= 2;
     }
 }
