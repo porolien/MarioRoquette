716274c55d893e6fead883cbb60ab8faaecad1ec
nathan-exe <nathan.tazi.1@gmail.com>
2023-12-21 12:12:34 +0100

    map3 nathan + tag recul camera pour triggers

--- Assets/cameraBehaviour.cs
+++ Assets/cameraBehaviour.cs
@@ -6,13 +6,16 @@ public class cameraBehaviour : MonoBehaviour
 {
     public GameObject TargetGameObject;
     public float targetY;
+    public float targetZ;
     public float smoothTime = 0.1f;
     public float previsionDuJOueur = 1;
     Vector3 vel;
+    
     // Start is called before the first frame update
     void Start()
     {
         targetY = transform.position.y;
+        targetZ = transform.position.z;
     }
 
     // Update is called once per frame
@@ -21,6 +24,7 @@ public class cameraBehaviour : MonoBehaviour
         Vector3 target = transform.position;
         target.x = TargetGameObject.transform.position.x + Mathf.Clamp( TargetGameObject.GetComponent<DynamicObject>().getVelocity().x,-8,8)* previsionDuJOueur;
         target.y = targetY;
+        target.z = targetZ;
 
         transform.position = Vector3.SmoothDamp(transform.position, target, ref vel, smoothTime);
     }
